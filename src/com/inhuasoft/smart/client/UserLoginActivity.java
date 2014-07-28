4cb7c76eaefe583d1fd5533deb903c6e4cd42d72
zwzhu <zwzhumail@163.com>
2014-07-28 15:14:36 +0800

    set iptel.org

--- src/com/inhuasoft/smart/client/UserLoginActivity.java
+++ src/com/inhuasoft/smart/client/UserLoginActivity.java
@@ -73,7 +73,8 @@ public class UserLoginActivity extends Activity implements OnClickListener {
 		 if(LinphonePreferences.instance().getAccountCount() < 1)
 		 {
 		   //LinphonePreferences.instance().CreatedAccount(editUserName.getEditableText().toString(),editPassword.getEditableText().toString(),"115.28.9.71");
-		     LinphonePreferences.instance().CreatedAccount("myitm","198863","opensips.org");
+		    // LinphonePreferences.instance().CreatedAccount("myitm","198863","opensips.org");
+		     LinphonePreferences.instance().CreatedAccount("myitm","198863","iptel.org");
 			 LinphonePreferences.instance().setInitiateVideoCall(true);
 			 LinphonePreferences.instance().setAutomaticallyAcceptVideoRequests(true);
 		  
